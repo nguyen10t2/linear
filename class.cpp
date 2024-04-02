b69cb64de34b5299fc6b5b1d356f1b99a8b68776
nguyen10t2 <123267700+nguyen10t2@users.noreply.github.com>
2024-04-03 00:45:22 +0700

    Add files via upload

--- class.cpp
+++ class.cpp
@@ -2,8 +2,8 @@
 #include <algorithm>
 #include <string>
 
-
-template <typename type> class stack
+template <typename type>
+class stack
 {
     struct Node
     {
@@ -20,7 +20,7 @@ private:
 public:
     struct Node *createNode(type data)
     {
-        struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+        struct Node *newNode = new Node();
         newNode->data = data;
         newNode->next = NULL;
         newNode->prev = NULL;
@@ -75,7 +75,9 @@ public:
             return false;
     }
 };
-template <typename type> class queue
+
+template <typename type>
+class queue
 {
     struct Node
     {
@@ -84,91 +86,165 @@ template <typename type> class queue
         struct Node *prev;
     };
 
-    private:
-        struct Node *head = NULL;
-        int leng = 0;
-        struct Node *lastNode = head;
+private:
+    struct Node *head = NULL;
+    int leng = 0;
+    struct Node *lastNode = head;
 
-    public:
-        struct Node *createNode(type data)
+public:
+    struct Node *createNode(type data)
+    {
+        struct Node *newNode = new Node();
+        newNode->data = data;
+        newNode->next = NULL;
+        newNode->prev = NULL;
+        return newNode;
+    }
+    void push(type data)
+    {
+        if (head == NULL)
         {
-            struct Node *newNode = new Node();
-            newNode->data = data;
-            newNode->next = NULL;
-            newNode->prev = NULL;
-            return newNode;
-        }
-        void push(type data)
-        {
-            if (head == NULL)
-            {
-                head = createNode(data);
-                ++leng;
-                lastNode = head;
-                return;
-            }
-            struct Node *newNode = createNode(data);
-            lastNode->next = newNode;
-            newNode->prev = lastNode;
-            lastNode = newNode;
+            head = createNode(data);
             ++leng;
+            lastNode = head;
+            return;
         }
-        void pop()
+        struct Node *newNode = createNode(data);
+        lastNode->next = newNode;
+        newNode->prev = lastNode;
+        lastNode = newNode;
+        ++leng;
+    }
+    void pop()
+    {
+        struct Node *temp = head;
+        head = head->next;
+        if (head == NULL)
+            lastNode = head;
+        free(temp);
+        --leng;
+    }
+    type front()
+    {
+        if (head == NULL)
+            return "";
+        return head->data;
+    }
+    int size()
+    {
+        return leng;
+    }
+    bool empty()
+    {
+        if (leng == 0)
+            return true;
+        return false;
+    }
+    void call()
+    {
+        struct Node *temp = head;
+        while (temp != NULL)
         {
-            struct Node *temp = head;
-            head = head->next;
-            if(head == NULL) lastNode = head;
-            free(temp);
-            --leng;
+            std::cout << temp->data << ' ';
+            temp = temp->next;
         }
-        type front()
-        {
-            if (head == NULL)
-                return "";
-            return head->data;
-        }
-        int size()
-        {
-            return leng;
-        }
-        bool empty()
-        {
-            if (leng == 0)
-                return true;
-            return false;
-        }
-        void call()
-        {
-            struct Node *temp = head;
-            while (temp != NULL)
-            {
-                std::cout << temp->data << ' ';
-                temp = temp->next;
-            }
-        }
+    }
 };
+
+template <typename type>
+class deque
+{
+    struct Node
+    {
+        type data;
+        struct Node* next;
+        struct Node* prev;
+    };
+private:
+    struct Node* head = NULL;
+    struct Node* lastNode = head;
+    int leng = 0;
+public:
+    struct Node* create(type data)
+    {
+        struct Node* newNode = new Node();
+        newNode->data = data;
+        newNode->next = NULL;
+        newNode->prev = NULL;
+        return newNode;
+    }
+    void push_back(type data){
+        if(head == NULL){
+            head = create(data);
+            lastNode = head;
+            ++leng;
+            return;
+        }
+        struct Node* newNode = create(data);
+        lastNode->next = newNode;
+        newNode->prev = lastNode;
+        lastNode = newNode;
+        ++leng;
+    }
+    void push_front(type data){
+        if(head == NULL){
+            head = create(data);
+            lastNode = head;
+            ++leng;
+            return;
+        }
+        struct Node* newNode = create(data);
+        newNode->next = head;
+        head->prev = newNode;
+        head = newNode;
+        ++leng;
+    }
+    void pop_back(){
+        struct Node *temp = lastNode->prev;
+        lastNode->prev->next = NULL;
+        free(lastNode);
+        lastNode = temp;
+        --leng;
+    }
+    void pop_front(){
+        struct Node* temp = head;
+        head = head->next;
+        if(head == NULL)
+        {
+            lastNode = head;
+        }
+        free(temp);
+        --leng;
+    }
+    type front()
+    {
+        return head->data;
+    }
+    type back()
+    {
+        return lastNode->data;
+    }
+    int size()
+    {
+        return leng;
+    }
+    bool empty()
+    {
+        if(leng == 0) return true;
+        return false;
+    }
+    void call()
+    {
+        struct Node* temp = head;
+        while(temp != NULL)
+        {
+            std::cout<<temp->data<<' ';
+            temp = temp->next;
+        }
+    }
+};
+
 int main()
 {
-    queue<std::string> p;
-    int n;
-    std::cin >> n;
-    std::string bin[n + 1];
-    bin[1] = "1";
-    p.push("1");
-    int count = 1;
-    while (count < n)
-    {
-        std::string top = p.front();
-        p.pop();
-        ++count;
-        bin[count] = top + "0";
-        p.push(bin[count]);
-        ++count;
-        bin[count] = top + "1";
-        p.push(bin[count]);
-    }
-    for (int i = 1; i <= n; ++i)
-    {
-        std::cout << bin[i] << '\n';
-    }
+    
 }
